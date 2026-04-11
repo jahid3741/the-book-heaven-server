c63faf039ebfe6e5a808f6d41ccb8542f7beb1fa
jahid3741 <mdj868445@gmail.com>
2026-04-11 23:24:15 +0600

    add delete book api

--- index.js
+++ index.js
@@ -78,6 +78,17 @@ async function run() {
       res.send(result);
     });
 
+    // delete book by id
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await booksCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
 
   } catch (error) {
     console.log(error);
