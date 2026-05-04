bf5d60e574ecbae99c002e613947fac8d8ede2a6
jahid3741 <mdj868445@gmail.com>
2026-05-04 23:18:50 +0600

    add all changes code

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// mongodb connection
 const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
@@ -31,54 +29,51 @@ async function run() {
 
     console.log("MongoDB connected");
 
-    // get all books
     app.get("/books", async (req, res) => {
       const result = await booksCollection.find().toArray();
       res.send(result);
     });
 
-    // add book (single or multiple)
-    app.post("/books", async (req, res) => {
-      const data = req.body;
-
-      let result;
-
-      if (Array.isArray(data)) {
-        result = await booksCollection.insertMany(data);
-      } else {
-        result = await booksCollection.insertOne(data);
-      }
-
-      res.send(result);
-    });
-
-    // get single book by id
     app.get("/books/:id", async (req, res) => {
       const id = req.params.id;
-
       const result = await booksCollection.findOne({
         _id: new ObjectId(id),
       });
-
       res.send(result);
     });
 
-    // update book by id
-    app.patch("/books/:id", async (req, res) => {
+    app.get("/my-books", async (req, res) => {
+      const email = req.query.email;
+      const result = await booksCollection.find({ userEmail: email }).toArray();
+      res.send(result);
+    });
+
+    app.get("/latest-books", async (req, res) => {
+      const result = await booksCollection
+        .find()
+        .sort({ _id: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/books", async (req, res) => {
+      const result = await booksCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    app.put("/books/:id", async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body;
 
       const result = await booksCollection.updateOne(
         { _id: new ObjectId(id) },
-        {
-          $set: updatedData,
-        },
+        { $set: updatedData },
       );
 
       res.send(result);
     });
 
-    // delete book by id
     app.delete("/books/:id", async (req, res) => {
       const id = req.params.id;
 
@@ -88,8 +83,6 @@ async function run() {
 
       res.send(result);
     });
-
-
   } catch (error) {
     console.log(error);
   }
@@ -97,12 +90,10 @@ async function run() {
 
 run().catch(console.dir);
 
-// root route
 app.get("/", (req, res) => {
   res.send("The Book Heaven Server is running");
 });
 
-// server start
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
